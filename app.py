69bed91c06629cfd1cc5b0c7d46f1fcd7bcfd878
Lex Nastin <lexnastin@gmail.com>
2023-07-16 23:42:46 +0300

    :shrug:

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import requests
 import os
 import telebot
+import datetime
 
 #app = Flask(__name__)
 
@@ -18,10 +19,13 @@ def main():
 	fouryears = requests.get("https://48.ie/panda/protected/users/339696/sims/893531118020807745/balance/summary/current", headers=headers).json()
 	remaining_roam = int(fouryears["_payload"]["current_month_balance_summary"]["left_fup_data"])
 	gib = remaining_roam / GIB2KIB
-	mb = round((gib * 1000) % 1000)
+	mb = round(((gib * 1000) % 1000) // 1)
 	kb = round((gib * 1000 * 1000) % 1000)
-	gib = round(gib)
-	return f"{gib}GiB {mb}MB {kb}KB"
+	days = datetime.datetime.now().day - 12
+	allowed = 10 - ( (10 / ( 26 - 13 ) ) * days )
+	allowed_more = round(gib - allowed, 3)
+	gib = round(gib // 1)
+	return f"{gib}GiB {mb}MB {kb}KB\nYou're allowed {allowed_more}GiB today"
 
 bot = telebot.TeleBot(TOKEN)
 
